b27cbf96fae7692cbf00ffcda8ba6f33146b26e4
Al Shaw <almshaw@gmail.com>
2010-08-12 23:57:48 -0400

    update for new version of 4sq oauth api which requires explicit callback url and oauth_verifier params. callback url set in-app to localhost:4567. version bump to 0.0.3

--- base.rb
+++ base.rb
@@ -1,136 +1,137 @@
 require 'yaml'
 
 module Fourrific
-	
-	VERSION = '0.0.2'
-	
-	class Authorize
-		
-		#get keys from YAML
-		f = File.open( 'credentials.yml' ) { |yf| YAML::load( yf ) }
-		CONSUMER_KEY = f['foursquare_keys']['key'].to_s
-		CONSUMER_SECRET = f['foursquare_keys']['secret'].to_s
-		
-		def initialize
-			@consumer = OAuth::Consumer.new(CONSUMER_KEY,CONSUMER_SECRET, {
-						 :site               => "http://foursquare.com",
-						 :scheme             => :header,
-						 :http_method        => :post,
-						 :request_token_path => "/oauth/request_token",
-						 :access_token_path  => "/oauth/access_token",
-						 :authorize_path     => "/oauth/authorize"
-						})
-			
-			@request_token=@consumer.get_request_token					
-		end
-		
-		def get_tokens
-			#store @request_token.token and @request_token.secret for use when you get the callback
-			#ask user to visit @request_token.authorize_url
-			
-			@tokens = {}
-			@tokens[:request_token] = @request_token.token
-			@tokens[:secret] = @request_token.secret
-			@tokens[:url] = @request_token.authorize_url
-			
-			@tokens
-		end
-		
-		
-		def access_token(oauth_token,secret)
-			#... in your callback page:
-			# request_token_key will be 'oauth_token' in the query paramaters of the incoming get request
-			
-			@request_token = OAuth::RequestToken.new(@consumer, oauth_token, secret)
-			@access_token=@request_token.get_access_token
-			
-			@access_tokens = {}
-			@access_tokens[:token] = @access_token.token
-			@access_tokens[:secret] = @access_token.secret
-			
-			@access_tokens
-			#store @access_token.token and @access_token.secret
-		end
-	
-	end
-	
-	class IPGeocode
-		
-		def initialize(ip)
-			# compensate for localhost
-			# http://www.commandlinefu.com/commands/view/1733/get-own-public-ip-address
-			if ip == "127.0.0.1"
-				ip = `curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'`.strip
-			end
-			
-			@g = Geokit::Geocoders::MultiGeocoder.geocode(ip)
-		end
-		
-		def ll
-			@ll = {}
-		
-			@ll[:lat] = @g.lat
-			@ll[:long] = @g.lng
-		
-			@ll
-		end
-		
-		def you_are_in
-			@g.city
-		end
-		
-	end
+  
+  VERSION = '0.0.3'
+  
+  class Authorize
+    
+    #get keys from YAML
+    f = File.open( 'credentials.yml' ) { |yf| YAML::load( yf ) }
+    CONSUMER_KEY = f['foursquare_keys']['key'].to_s
+    CONSUMER_SECRET = f['foursquare_keys']['secret'].to_s
+    
+    def initialize
+      @consumer = OAuth::Consumer.new(CONSUMER_KEY,CONSUMER_SECRET, {
+             :site               => "http://foursquare.com",
+             :scheme             => :header,
+             :http_method        => :post,
+             :request_token_path => "/oauth/request_token",
+             :access_token_path  => "/oauth/access_token",
+             :authorize_path     => "/oauth/authorize"
+            })
+                                                  # add callback URL, or 4sq will ask you to
+                                                  # enter a "pin" number in the app
+      @request_token=@consumer.get_request_token :oauth_callback => "http://localhost:4567/"      
+    end
+    
+    def get_tokens
+      #store @request_token.token and @request_token.secret for use when you get the callback
+      #ask user to visit @request_token.authorize_url
+      
+      @tokens = {}
+      @tokens[:request_token] = @request_token.token
+      @tokens[:secret] = @request_token.secret
+      @tokens[:url] = @request_token.authorize_url
+      
+      @tokens
+    end
+    
+    
+    def access_token(oauth_token,secret,oauth_verifier)
+      #... in your callback page:
+      # request_token_key will be 'oauth_token' in the query paramaters of the incoming get request
+      
+      @request_token = OAuth::RequestToken.new(@consumer, oauth_token, secret)
+      @access_token=@request_token.get_access_token :oauth_verifier => oauth_verifier
+      
+      @access_tokens = {}
+      @access_tokens[:token] = @access_token.token
+      @access_tokens[:secret] = @access_token.secret
+      
+      @access_tokens
+      #store @access_token.token and @access_token.secret
+    end
+  
+  end
+  
+  class IPGeocode
+    
+    def initialize(ip)
+      # compensate for localhost
+      # http://www.commandlinefu.com/commands/view/1733/get-own-public-ip-address
+      if ip == "127.0.0.1"
+        ip = `curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'`.strip
+      end
+      
+      @g = Geokit::Geocoders::MultiGeocoder.geocode(ip)
+    end
+    
+    def ll
+      @ll = {}
+    
+      @ll[:lat] = @g.lat
+      @ll[:long] = @g.lng
+    
+      @ll
+    end
+    
+    def you_are_in
+      @g.city
+    end
+    
+  end
 
-	class Checkins		
-		def initialize(access,secret)
-			
-			@consumer = OAuth::Consumer.new(Fourrific::Authorize::CONSUMER_KEY,Fourrific::Authorize::CONSUMER_SECRET, {
-						 :site               => "http://api.foursquare.com",
-						 :scheme             => :header,
-						 :http_method        => :post,
-						})	
-						
-			@access_token = OAuth::AccessToken.new(@consumer, access, secret)			
-		end
-		
-		#g is a geocode array from IPGeocode.new
-		def friends(g)
-			
-			ip = g.ll
-			@friends = @access_token.get("/v1/checkins?geolat=#{ip[:lat]}&geolong=#{ip[:long]}", {'User-Agent' => "fourrific:#{Fourrific::VERSION}"}).body	
-			
-			begin
-				@friends = Crack::XML.parse(@friends)
-				if @friends['unauthorized']
-					@error = "#{@friends['unauthorized']}. Clear your cookies & cache and try again."
-				elsif @friends['ratelimited']
-					@error = "#{@friends['ratelimited']}. Please try again later."
-				elsif @friends['error']
-					@error = "#{@friends['error']}"
-				else
-					@friends['checkins']['checkin'].each do |checkin|
-						checkin['created'] = checkin['created'].to_time.iso8601
-						checkin['distance'] = (checkin['distance'].to_i / 1609.344).to_i
-					end
-				end				
-			end
-			
-	
-			@friends
-		
-		end
-		
-		def first_is_far?
-			checkin = @friends['checkins']['checkin'][0]
-			return true if checkin['distance'] > 25
-		end
-		
-		def error
-			@error 
-		end
-		
-	end
-	
+  class Checkins    
+    def initialize(access,secret)
+      
+      @consumer = OAuth::Consumer.new(Fourrific::Authorize::CONSUMER_KEY,Fourrific::Authorize::CONSUMER_SECRET, {
+             :site               => "http://api.foursquare.com",
+             :scheme             => :header,
+             :http_method        => :post,
+            })  
+            
+      @access_token = OAuth::AccessToken.new(@consumer, access, secret)     
+    end
+    
+    #g is a geocode array from IPGeocode.new
+    def friends(g)
+      
+      ip = g.ll
+      @friends = @access_token.get("/v1/checkins?geolat=#{ip[:lat]}&geolong=#{ip[:long]}", {'User-Agent' => "fourrific:#{Fourrific::VERSION}"}).body  
+      
+      begin
+        @friends = Crack::XML.parse(@friends)
+        if @friends['unauthorized']
+          @error = "#{@friends['unauthorized']}. Clear your cookies & cache and try again."
+        elsif @friends['ratelimited']
+          @error = "#{@friends['ratelimited']}. Please try again later."
+        elsif @friends['error']
+          @error = "#{@friends['error']}"
+        else
+          @friends['checkins']['checkin'].each do |checkin|
+            checkin['created'] = checkin['created'].to_time.iso8601
+            checkin['distance'] = (checkin['distance'].to_i / 1609.344).to_i
+          end
+        end       
+      end
+      
+  
+      @friends
+    
+    end
+    
+    def first_is_far?
+      checkin = @friends['checkins']['checkin'][0]
+      return true if checkin['distance'] > 25
+    end
+    
+    def error
+      @error 
+    end
+    
+  end
+  
 
 end
 
--- fourrific.rb
+++ fourrific.rb
@@ -15,67 +15,67 @@ require File.dirname(__FILE__) + '/base'
 enable :sessions
 
 get '/' do
-	@ip = @env['REMOTE_ADDR']
-	
-	if params[:oauth_token]
-		h = Fourrific::Authorize.new
-		t = h.access_token(params[:oauth_token],session[:request_secret])
-		session[:token] = t[:token]
-		session[:secret] = t[:secret]
-		
-		#if you reload the page with the oauth param, you 500, better to redirect and kill the possibility
-		redirect '/' 
-		
-	elsif session[:token].nil?
-		redirect '/login'	
-	end
-		
-	@g = Fourrific::IPGeocode.new(@ip)
-	@city = @g.you_are_in
+  @ip = @env['REMOTE_ADDR']
+  
+  if params[:oauth_token]
+    h = Fourrific::Authorize.new
+    t = h.access_token(params[:oauth_token],session[:request_secret], params[:oauth_verifier])
+    session[:token] = t[:token]
+    session[:secret] = t[:secret]
+    
+    #if you reload the page with the oauth param, you 500, better to redirect and kill the possibility
+    redirect '/' 
+    
+  elsif session[:token].nil?
+    redirect '/login' 
+  end
+    
+  @g = Fourrific::IPGeocode.new(@ip)
+  @city = @g.you_are_in
 
-	begin
-		c = Fourrific::Checkins.new(session[:token],session[:secret])
-		@c = c.friends(@g)
-		if c.error && c.error.length > 0
-			raise c.error
-		end
-	end
-	
-	if c.first_is_far?
-		@first_is_far = true
-	end
-	
-	erb :index
+  begin
+    c = Fourrific::Checkins.new(session[:token],session[:secret])
+    @c = c.friends(@g)
+    if c.error && c.error.length > 0
+      raise c.error
+    end
+  end
+  
+  if c.first_is_far?
+    @first_is_far = true
+  end
+  
+  erb :index
 
 end
 
 get '/login' do
-	
-	h = Fourrific::Authorize.new
-	t = h.get_tokens
-	
-	session[:request_token] = t[:request_token]
-	session[:request_secret] = t[:secret]
-	@url = t[:url]
-	
-	redirect "#{@url}"
-	erb :login
+  
+  h = Fourrific::Authorize.new
+  t = h.get_tokens
+  
+  session[:request_token] = t[:request_token]
+  session[:request_secret] = t[:secret]
+  @url = t[:url]
+  
+  redirect "#{@url}"
+  erb :login
 end
 
 error do
-	@msg = env['sinatra.error']
-	
-	erb :unauthorized
+  @msg = env['sinatra.error']
+  
+  erb :unauthorized
 end
 
 error 404 do
-	@msg = "404. That page doesn't exist. <a href='/'>Go home</a>."
-	
-	erb :unauthorized
+  @msg = "404. That page doesn't exist. <a href='/'>Go home</a>."
+  
+  erb :unauthorized
 end
 
 error OAuth::Unauthorized do
-	@msg = "For some reason, I can't log you into foursquare. Try clearing your cache and cookies, and <a href='/'>starting over</a>."
-	
-	erb :unauthorized
+  @msg = "For some reason, I can't log you into foursquare. Try clearing your cache and cookies, and <a href='/'>starting over</a>."
+  
+  erb :unauthorized
 end
